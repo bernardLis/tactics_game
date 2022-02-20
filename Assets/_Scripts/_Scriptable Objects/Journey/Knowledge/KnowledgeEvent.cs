8d6eb6d87066b0bb45e73b83b83282afa44407fa
Bernard Lis <bernard5lis@gmail.com>
2022-02-20 18:52:24 +0100

    removed knowledge, I might come back to it some day

--- Assets/_Scripts/_Scriptable Objects/Journey/Knowledge/KnowledgeEvent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObject/Journey/KnowledgeEvent")]
-public class KnowledgeEvent : BaseScriptableObject
-{
-    public KnowledgeEventOption[] options;
-
-}
