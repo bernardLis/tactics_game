e0336567b6efdc216cdc39ffe0fbb4937a15c6d9
blis <blis@hey.com>
2023-01-07 12:29:08 +0100

    moved styles out of character card to uss file

--- Assets/_Scripts/_UIElements/Character/CharacterCard.cs
+++ Assets/_Scripts/_UIElements/Character/CharacterCard.cs
@@ -27,30 +27,47 @@ public class CharacterCard : VisualElement
 
     VisualElement _levelUpAnimationContainer;
 
+    const string _ussCommonTextPrimary = "common__text-primary";
+    const string _ussCommonTransitionBasic = "common__transition-basic";
+
+
+    const string _ussClassName = "character-card";
+    const string _ussMain = _ussClassName + "__main";
+    const string _ussTopPanel = _ussClassName + "__top-panel";
+    const string _ussPortraitContainer = _ussClassName + "__portrait-container";
+    const string _ussBottomPanel = _ussClassName + "__bottom-panel";
+    const string _ussStatContainer = _ussClassName + "__stat-container";
+    const string _ussExpContainer = _ussClassName + "__exp-container";
+    const string _ussHealthContainer = _ussClassName + "__health-container";
+    const string _ussManaContainer = _ussClassName + "__mana-container";
+    const string _ussOverlay = _ussClassName + "__overlay";
+
+
     public CharacterCard(Character character, bool showExp = true, bool showAbilities = true, bool showItems = true)
     {
+        var commonStyles = GameManager.Instance.GetComponent<AddressableManager>().GetStyleSheetByName(StyleSheetType.CommonStyles);
+        if (commonStyles != null)
+            styleSheets.Add(commonStyles);
+        var ss = GameManager.Instance.GetComponent<AddressableManager>().GetStyleSheetByName(StyleSheetType.CharacterCardStyles);
+        if (ss != null)
+            styleSheets.Add(ss);
+
         Character = character;
         Character.ResolveItems();
 
-        AddToClassList("characterCard");
-        AddToClassList("textPrimary");
+        AddToClassList(_ussMain);
+        AddToClassList(_ussCommonTextPrimary);
 
         VisualElement topPanel = new();
-        topPanel.style.flexDirection = FlexDirection.Row;
-        topPanel.style.alignItems = Align.Center;
-        topPanel.style.justifyContent = Justify.Center;
+        topPanel.AddToClassList(_ussTopPanel);
 
         VisualElement portraitContainer = new();
-        portraitContainer.style.alignItems = Align.Center;
-        portraitContainer.style.marginRight = 20;
+        portraitContainer.AddToClassList(_ussPortraitContainer);
 
         PortraitVisualElement = new(character, this);
-        Label name = new($"{character.CharacterName}");
-
-        _title = new($"{Character.Rank.Title}");
-
         portraitContainer.Add(PortraitVisualElement);
-        portraitContainer.Add(name);
+
+        _title = new($"[{Character.Rank.Title}] {character.CharacterName}");
         portraitContainer.Add(_title);
 
         VisualElement barsContainer = new();
@@ -64,9 +81,7 @@ public class CharacterCard : VisualElement
         topPanel.Add(barsContainer);
 
         VisualElement bottomPanel = new();
-        bottomPanel.style.flexDirection = FlexDirection.Row;
-        bottomPanel.style.alignItems = Align.Center;
-        bottomPanel.style.justifyContent = Justify.Center;
+        bottomPanel.AddToClassList(_ussBottomPanel);
 
         if (showAbilities)
             bottomPanel.Add(CreateAbilities());
@@ -76,7 +91,7 @@ public class CharacterCard : VisualElement
         Add(topPanel);
         Add(bottomPanel);
 
-        AvailabilityCheck(); // for armory
+        AvailabilityCheck();
         Character.OnRankChanged += OnRankChanged;
         SubscribeToStatChanges();
     }
@@ -90,16 +105,13 @@ public class CharacterCard : VisualElement
     VisualElement CreateRankElement()
     {
         _rankElement = new(Character.Rank.Rank, 0.5f);
-
         return _rankElement;
     }
 
     VisualElement CreateStatGroup()
     {
         VisualElement container = new();
-        container.style.flexDirection = FlexDirection.Row;
-        container.style.alignContent = Align.Center;
-        container.style.width = Length.Percent(100);
+        container.AddToClassList(_ussStatContainer);
 
         GameDatabase db = GameManager.Instance.GameDatabase;
         _power = new(db.GetStatIconByName("Power"), Character.GetStatValue("Power"), "Power");
@@ -116,14 +128,15 @@ public class CharacterCard : VisualElement
     VisualElement CreateExpGroup()
     {
         VisualElement container = new();
-        container.style.alignContent = Align.Center;
-        container.style.width = Length.Percent(100);
+        container.AddToClassList(_ussExpContainer);
 
         ExpBar = new(Color.black, "Experience", 100, Character.Experience, 0, true);
+
         _level = new Label($"Level {Character.Level}");
         _level.style.position = Position.Absolute;
         _level.AddToClassList("textSecondary");
         ExpBar.Add(_level);
+
         Character.OnCharacterExpGain += OnExpChange;
         Character.OnCharacterLevelUp += OnLevelUp;
 
@@ -133,26 +146,24 @@ public class CharacterCard : VisualElement
 
     VisualElement CreateHealthGroup()
     {
-        VisualElement healthGroup = new();
-        healthGroup.style.flexDirection = FlexDirection.Row;
-        healthGroup.style.width = Length.Percent(100);
+        VisualElement container = new();
+        container.AddToClassList(_ussHealthContainer);
 
         HealthBar = new(Helpers.GetColor("healthBarRed"), "Health", Character.GetStatValue("MaxHealth"), Character.GetStatValue("MaxHealth"));
-        healthGroup.Add(HealthBar);
+        container.Add(HealthBar);
 
-        return healthGroup;
+        return container;
     }
 
     VisualElement CreateManaGroup()
     {
-        VisualElement manaGroup = new();
-        manaGroup.style.flexDirection = FlexDirection.Row;
-        manaGroup.style.width = Length.Percent(100);
+        VisualElement container = new();
+        container.AddToClassList(_ussManaContainer);
 
         ManaBar = new(Helpers.GetColor("manaBarBlue"), "Mana", Character.GetStatValue("MaxMana"), Character.GetStatValue("MaxMana"));
-        manaGroup.Add(ManaBar);
+        container.Add(ManaBar);
 
-        return manaGroup;
+        return container;
     }
 
     void OnExpChange(int expGain) { ExpBar.OnValueChanged(expGain, 3000); }
@@ -211,8 +222,6 @@ public class CharacterCard : VisualElement
 
         for (int i = 0; i < Character.Abilities.Count; i++)
         {
-            if (i > slotCount)
-                break;
             AbilityButton abilityButton = new AbilityButton(Character.Abilities[i], null);
             AbilitySlots[i].AddButton(abilityButton);
             AbilityButtons.Add(abilityButton);
@@ -229,16 +238,10 @@ public class CharacterCard : VisualElement
         VisualElement overlay = new VisualElement();
         Add(overlay);
         overlay.BringToFront();
-        overlay.style.position = Position.Absolute;
-        overlay.style.width = Length.Percent(100);
-        overlay.style.height = Length.Percent(100);
-        overlay.style.alignSelf = Align.Center;
-        overlay.style.alignItems = Align.Center;
-        overlay.style.justifyContent = Justify.Center;
-        overlay.style.backgroundColor = new StyleColor(new Color(0, 0, 0, 0.5f));
+        overlay.AddToClassList(_ussOverlay);
 
         Label text = new($"Unavailable! ({Character.UnavailabilityDuration})");
-        text.AddToClassList("textPrimary");
+        text.AddToClassList(_ussCommonTextPrimary);
         text.style.fontSize = 60;
         text.transform.rotation *= Quaternion.Euler(0f, 0f, 30f);
         overlay.Add(text);
