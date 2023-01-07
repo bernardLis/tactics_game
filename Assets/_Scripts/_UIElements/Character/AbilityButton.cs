4454d929a8a4b00ffd14078e290233dfabfc26c3
blis <blis@hey.com>
2023-01-07 14:01:28 +0100

    moved ability button styles from common styles

--- Assets/_Scripts/_UIElements/Character/AbilityButton.cs
+++ Assets/_Scripts/_UIElements/Character/AbilityButton.cs
@@ -3,34 +3,46 @@ using UnityEngine;
 
 public class AbilityButton : ElementWithSound
 {
-
     AbilityIcon _icon;
 
     public string Key;
     public Ability Ability;
 
+    const string _ussCommonTextPrimary = "common__text-primary";
+
+    const string _ussClassName = "ability-button";
+
+    const string _ussMain = _ussClassName + "__main";
+    const string _ussStat = _ussClassName + "__stat";
+    const string _ussContainer = _ussClassName + "__container";
+
     public AbilityButton(Ability ability, string key = null) : base()
     {
+        var commonStyles = GameManager.Instance.GetComponent<AddressableManager>().GetStyleSheetByName(StyleSheetType.CommonStyles);
+        if (commonStyles != null)
+            styleSheets.Add(commonStyles);
+        var ss = GameManager.Instance.GetComponent<AddressableManager>().GetStyleSheetByName(StyleSheetType.AbilityButtonStyles);
+        if (ss != null)
+            styleSheets.Add(ss);
+
         Ability = ability;
         Key = key;
 
         style.backgroundColor = ability.HighlightColor;
-        AddToClassList("abilityButton");
-        AddToClassList("textPrimary");
+        AddToClassList(_ussMain);
+        AddToClassList(_ussCommonTextPrimary);
 
         _icon = new AbilityIcon(ability, key);
         Add(_icon);
 
         TextWithTooltip baseDamage = new TextWithTooltip("" + Ability.BasePower, "Base damage");
-        baseDamage.AddToClassList("abilityButtonStat");
+        baseDamage.AddToClassList(_ussStat);
 
         TextWithTooltip manaCost = new TextWithTooltip("" + Ability.ManaCost, "Mana cost");
-        manaCost.AddToClassList("abilityButtonStat");
+        manaCost.AddToClassList(_ussStat);
 
         VisualElement container = new();
-
-        container.style.flexDirection = FlexDirection.Column;
-        container.style.justifyContent = Justify.SpaceAround;
+        container.AddToClassList(_ussContainer);
         Add(container);
 
         container.Add(baseDamage);
