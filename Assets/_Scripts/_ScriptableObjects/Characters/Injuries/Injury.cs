119fd6e7e40a0f7fd01522306939386995d2e52a
blis <blis@hey.com>
2023-02-17 18:55:29 +0100

    added injuries

--- Assets/_Scripts/_ScriptableObjects/Characters/Injuries/Injury.cs
+++ Assets/_Scripts/_ScriptableObjects/Characters/Injuries/Injury.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
+[CreateAssetMenu(menuName = "ScriptableObject/Character/Injury")]
 public class Injury : BaseScriptableObject
 {
+    public float DaysInjured;
+
 }
