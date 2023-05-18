8c5b4c1ac972ca90544a8fea728560a20b84073f
blis <blis@hey.com>
2023-05-18 19:09:07 +0200

    evolution show works alright!

--- Assets/_Scripts/Battle/BattleEndManager.cs
+++ Assets/_Scripts/Battle/BattleEndManager.cs
@@ -6,17 +6,31 @@ using DG.Tweening;
 
 public class BattleEndManager : MonoBehaviour
 {
+    const string _ussClassName = "battle__";
+    const string _ussEvolutionInfoContainer = _ussClassName + "evolution-info-container";
+
     GameManager _gameManager;
     Camera _cam;
     BattleManager _battleManager;
 
     [SerializeField] Canvas _battleCanvas;
 
+    [SerializeField] Shader _dissolveShader;
+
     VisualElement _root;
+
+    VisualElement _infoContainer;
+    VisualElement _entityElementContainer;
+
+    EntityElement _entityElement;
 
     List<ArmyGroup> _playerArmy;
 
+
+    ArmyGroup _currentArmyGroup;
     BattleEntity _currentEntity;
+    BattleEntity _evolvedEntity;
+
 
     void Start()
     {
@@ -55,6 +69,7 @@ public class BattleEndManager : MonoBehaviour
 
         foreach (ArmyGroup ag in _playerArmy)
         {
+            _currentArmyGroup = ag;
             yield return ShowArmyGroup(ag);
         }
         ShowUI();
@@ -69,19 +84,9 @@ public class BattleEndManager : MonoBehaviour
     IEnumerator ShowArmyGroup(ArmyGroup ag)
     {
         // HERE: testing
-        ag.KillCount = Random.Range(0, 10);
+        ag.KillCount = Random.Range(5, 10);
 
-        // for each of player army, I want to spawn one entity
-        ArmyEntity entityInstance = Instantiate(ag.ArmyEntity);
-        entityInstance.HeroInfluence(_gameManager.PlayerHero);
-
-        Vector3 pos = Vector3.zero;
-        pos.y = 1f;
-        GameObject instance = Instantiate(ag.ArmyEntity.Prefab, pos, Quaternion.identity);
-        _currentEntity = instance.GetComponent<BattleEntity>();
-        List<BattleEntity> emptyList = new();
-        _currentEntity.Initialize(true, entityInstance, ref emptyList);
-        _currentEntity.StopRunEntityCoroutine();
+        _currentEntity = InstantiateEntity(ag.ArmyEntity);
         // does not wokr  _currentEntity.Animator.SetBool("Move", true); 
         // TODO: ideally there is a cool animation list that is going on
 
@@ -89,44 +94,40 @@ public class BattleEndManager : MonoBehaviour
         _currentEntity.Animator.SetBool("Move", true);
 
         // HERE: you can make an element out of it, and every x call next method
-        VisualElement container = new();
-        container.style.position = Position.Absolute;
-        container.style.alignItems = Align.Center;
-        container.style.alignContent = Align.Center;
-        container.style.justifyContent = Justify.FlexStart;
-        container.style.left = 50;
-        container.style.top = 20;
-        container.style.height = Length.Percent(100);
-        container.style.minWidth = 300;
+        _infoContainer = new();
+        _infoContainer.AddToClassList(_ussEvolutionInfoContainer);
+        _root.Add(_infoContainer);
 
-        _root.Add(container);
+        _entityElementContainer = new();
+        _entityElementContainer.style.flexDirection = FlexDirection.Row;
+        _infoContainer.Add(_entityElementContainer);
 
-        EntityElement el = new(ag.ArmyEntity);
-        el.style.backgroundColor = Color.white;
-        container.Add(el);
-        el.style.opacity = 0;
-        DOTween.To(x => el.style.opacity = x, 0, 1, 0.5f);
 
-        yield return new WaitForSeconds(2f);
+        _entityElement = new(ag.ArmyEntity);
+        _entityElementContainer.Add(_entityElement);
+        _entityElement.style.opacity = 0;
+        DOTween.To(x => _entityElement.style.opacity = x, 0, 1, 0.5f);
 
-        container.Add(new Label("Kills to evolve:"));
+        yield return new WaitForSeconds(1f);
+
+        _infoContainer.Add(new Label("Kills to evolve:"));
 
         IntVariable kills = ScriptableObject.CreateInstance<IntVariable>();
         kills.SetValue(ag.OldKillCount);
 
         IntVariable killToEvolve = ScriptableObject.CreateInstance<IntVariable>();
         killToEvolve.SetValue(ag.NumberOfKillsToEvolve());
-
-        ResourceBarElement killBar = new(Color.white, "Kills", kills, killToEvolve);
+        Color barColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        ResourceBarElement killBar = new(barColor, "Kills", kills, killToEvolve);
         killBar.style.width = 200;
-        container.Add(killBar);
+        _infoContainer.Add(killBar);
         killBar.style.opacity = 0;
         DOTween.To(x => killBar.style.opacity = x, 0, 1, 0.5f);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         VisualElement killCountContainer = new();
-        container.Add(killCountContainer);
+        _infoContainer.Add(killCountContainer);
 
         killCountContainer.Add(new Label("Kills this battle:"));
         ChangingValueElement cv = new();
@@ -134,18 +135,17 @@ public class BattleEndManager : MonoBehaviour
 
         killCountContainer.Add(cv);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         int killChange = Mathf.Clamp(ag.KillCount - ag.OldKillCount, 0, ag.NumberOfKillsToEvolve());
 
-        cv.ChangeAmount(killChange);
+        cv.ChangeAmount(ag.KillCount - ag.OldKillCount - killChange);
 
         kills.ApplyChange(killChange);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         if (ag.ShouldEvolve())
             yield return Evolve();
-
 
         // center camera on it
         while (true)
@@ -154,8 +154,58 @@ public class BattleEndManager : MonoBehaviour
 
     IEnumerator Evolve()
     {
-        // fade out the 
+        Material mat = _currentEntity.GetComponentInChildren<Renderer>().material;
+        Texture2D tex = mat.mainTexture as Texture2D;
+
+        mat.shader = _dissolveShader;
+        mat.SetTexture("_Base_Texture", tex);
+
+        DOTween.To(x => mat.SetFloat("_Dissolve_Value", x), 0, 1, 5f);
+        yield return new WaitForSeconds(1f);
+
+        _currentArmyGroup.Evolve();
+
+        _evolvedEntity = InstantiateEntity(_currentArmyGroup.ArmyEntity);
+        _evolvedEntity.Collider.enabled = false;
+
+        Material evolvedMat = _evolvedEntity.GetComponentInChildren<Renderer>().material;
+        Texture2D evolvedTex = evolvedMat.mainTexture as Texture2D;
+
+        evolvedMat.shader = _dissolveShader;
+        evolvedMat.SetTexture("_Base_Texture", evolvedTex);
+
+        mat.SetFloat("_Dissolve_Value", 1f);
+        DOTween.To(x => evolvedMat.SetFloat("_Dissolve_Value", x), 1, 0, 5f)
+                .OnComplete(() => _evolvedEntity.Animator.SetBool("Move", true));
+
+        _infoContainer.Add(new Label("Evolved!"));
+        //   EntityElement evolvedElement = new(_currentArmyGroup.ArmyEntity);
+        _entityElement.SetValues(_currentArmyGroup.ArmyEntity);
+        //   _entityElementContainer.Add(evolvedElement);
+        //   evolvedElement.style.opacity = 0;
+        //    DOTween.To(x => evolvedElement.style.opacity = x, 0, 1, 0.5f);
+
+        yield return new WaitForSeconds(5f);
+
         yield return null;
+    }
+
+    BattleEntity InstantiateEntity(ArmyEntity armyEntity)
+    {
+        // for each of player army, I want to spawn one entity
+        ArmyEntity entityInstance = Instantiate(armyEntity);
+        entityInstance.HeroInfluence(_gameManager.PlayerHero);
+
+        Vector3 pos = Vector3.zero;
+        pos.y = 1f;
+        GameObject instance = Instantiate(armyEntity.Prefab, pos, Quaternion.identity);
+        BattleEntity be = instance.GetComponent<BattleEntity>();
+        List<BattleEntity> emptyList = new();
+        be.Initialize(true, entityInstance, ref emptyList);
+        be.StopRunEntityCoroutine();
+
+        return be;
+
     }
 
     IEnumerator RotateCameraAroundEntity()
@@ -166,7 +216,7 @@ public class BattleEndManager : MonoBehaviour
             {
                 _cam.transform.LookAt(_currentEntity.transform.position);
                 _cam.transform.parent.RotateAround(_currentEntity.transform.position,
-                        Vector3.up, 0.5f * Time.deltaTime);
+                        Vector3.up, -0.5f * Time.deltaTime);
             }
             yield return null;
         }
--- Assets/_Scripts/_UIElements/Army Entity/EntityElement.cs
+++ Assets/_Scripts/_UIElements/Army Entity/EntityElement.cs
@@ -8,6 +8,7 @@ public class EntityElement : VisualElement
     const string _ussClassName = "entity-element__";
     const string _ussMain = _ussClassName + "main";
     const string _ussElement = _ussClassName + "element";
+    const string _ussName = _ussClassName + "name";
 
     GameManager _gameManager;
 
@@ -47,13 +48,15 @@ public class EntityElement : VisualElement
         _elementalElement.style.bottom = 0;
 
         _entityIcon = new(_entity);
-        _nameLabel = new Label($"Name: {_entity.Name}");
-        _healthLabel = new Label($"Health: {_entity.Health}");
-        _power = new Label($"Power: {_entity.Power}");
-        _armor = new Label($"Armor: {_entity.Armor}");
-        _attackRange = new Label($"Attack Range: {_entity.AttackRange}");
-        _attackCooldown = new Label($"Attack Cooldown: {_entity.AttackCooldown}");
-        _speed = new Label($"Speed: {_entity.Speed}");
+        _nameLabel = new Label();
+        _nameLabel.AddToClassList(_ussName);
+        
+        _healthLabel = new Label($"Health:");
+        _power = new Label($"Power:");
+        _armor = new Label($"Armor:");
+        _attackRange = new Label($"Attack Range:");
+        _attackCooldown = new Label($"Attack Cooldown:");
+        _speed = new Label($"Speed:");
 
         _leftContainer.Add(_nameLabel);
         _leftContainer.Add(_healthLabel);
@@ -66,5 +69,21 @@ public class EntityElement : VisualElement
         _rightContainer.Add(_attackRange);
         _rightContainer.Add(_attackCooldown);
         _rightContainer.Add(_speed);
+
+        SetValues(_entity);
+    }
+
+    public void SetValues(ArmyEntity entity)
+    {
+        _entity = entity;
+
+        _entityIcon.SwapEntity(entity);
+        _nameLabel.text = $"{entity.Name}";
+        _healthLabel.text = $"Health: {entity.Health}";
+        _power.text = $"Power: {entity.Power}";
+        _armor.text = $"Armor: {entity.Armor}";
+        _attackRange.text = $"Attack Range: {entity.AttackRange}";
+        _attackCooldown.text = $"Attack Cooldown: {entity.AttackCooldown}";
+        _speed.text = $"Speed: {entity.Speed}";
     }
 }
