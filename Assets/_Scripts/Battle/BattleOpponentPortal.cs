0fc239e8934943d8c135778cf7a97f4aab860f23
blis <blis@hey.com>
2023-08-01 14:32:15 +0200

    placed elemental portals with effects

---
+++ Assets/_Scripts/Battle/BattleOpponentPortal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleOpponentPortal : MonoBehaviour
+{
+    [SerializeField] GameObject _portalEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
