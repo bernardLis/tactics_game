931c364988fba896fe3c45b1c441c85e9880cd11
blis <blis@hey.com>
2023-08-18 12:25:24 +0200

    not displaying creature toolip/info when it is dead

--- Assets/_Scripts/Battle/Entities/BattleEntityTooltipDisplayer.cs
+++ Assets/_Scripts/Battle/Entities/BattleEntityTooltipDisplayer.cs
@@ -13,22 +13,31 @@ public class BattleEntityTooltipDisplayer : MonoBehaviour, IPointerDownHandler, 
         _battleEntity = GetComponent<BattleEntity>();
         _tooltipManager = BattleTooltipManager.Instance;
     }
-    
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (!CanDisplayTooltip()) return;
         _tooltipManager.ShowTooltip(_battleEntity);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (_tooltipManager == null) return;
+        if (!CanDisplayTooltip()) return;
         _tooltipManager.ShowInfo(_battleEntity);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (_tooltipManager == null) return;
+        if (!CanDisplayTooltip()) return;
         _tooltipManager.HideInfo();
+    }
+
+    bool CanDisplayTooltip()
+    {
+        if (_tooltipManager == null) return false;
+        if (_battleEntity.IsDead) return false;
+
+        return true;
     }
 
 }
