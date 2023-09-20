f75e2760660aa6af6a260aa44929ac0e46322c41
blis <blis@hey.com>
2023-09-20 21:29:06 +0200

    wind tornado ability wokrs!

---
+++ Assets/_Scripts/Battle/Hero/Abilities/BattleTornado.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTornado : MonoBehaviour
+{
+
+    [SerializeField] float _speed = 5f;
+    Ability _ability;
+
+    public void Initialize(Ability ability)
+    {
+        _ability = ability;
+
+        StartCoroutine(GoForward(3f)); // TODO: hardcoded duration
+        Invoke(nameof(DestroySelf), 3.2f);
+    }
+
+    IEnumerator GoForward(float duration)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < duration)
+        {
+            transform.position += transform.forward * Time.deltaTime * _speed;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out BattleEntity battleEntity))
+        {
+            if (battleEntity.Team == 0) return; // TODO: hardcoded team number
+
+            StartCoroutine(battleEntity.GetHit(_ability));
+        }
+    }
+
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+}
