069f61b4e9a816b4616388c9453491aeaf1ba379
blis <blis@hey.com>
2023-11-13 16:35:24 +0100

    skybox for tests

---
+++ Assets/_Scripts/Battle/BattleCloudRotation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BattleCloudRotation : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), 120f, RotateMode.FastBeyond360)
+                 .SetLoops(-1);
+    }
+
+}
