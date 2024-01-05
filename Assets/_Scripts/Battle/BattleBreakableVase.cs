e59d245cf98a03f4197207b5bb75d68c44318347
blis <blis@hey.com>
2024-01-05 11:57:06 +0100

    moved and tested pickups

--- Assets/_Scripts/Battle/BattleBreakableVase.cs
+++ Assets/_Scripts/Battle/BattleBreakableVase.cs
@@ -13,13 +13,6 @@ public class BattleBreakableVase : MonoBehaviour
     [SerializeField] GameObject _breakParticles;
     [SerializeField] Transform _originalVase;
 
-    [SerializeField] Coin _coin;
-    [SerializeField] Hammer _hammer;
-    [SerializeField] Horseshoe _horseshoe;
-    [SerializeField] Bag _bag;
-    [SerializeField] Skull _skull;
-
-
     Collider _collider;
     Rigidbody _rigidbody;
 
@@ -73,87 +66,11 @@ public class BattleBreakableVase : MonoBehaviour
             _breakParticles.SetActive(true);
 
         _originalVase.gameObject.SetActive(false);
-        SpawnPickup();
+        _battlePickupManager.SpawnPickup(transform.position);
 
         OnBroken?.Invoke();
 
         yield return new WaitForSeconds(5f);
         gameObject.SetActive(false);
-    }
-
-    void SpawnPickup()
-    {
-        // 1% chance of spawning hammer
-        // 1% chance of spawning horseshoe
-        // 98% chance of spawning coin
-        int random = Random.Range(0, 100);
-        Pickup p = Instantiate(_coin);
-
-        if (random == 0)
-            p = Instantiate(_hammer);
-        else if (random == 1)
-            p = Instantiate(_horseshoe);
-        else if (random == 2)
-            p = Instantiate(_bag);
-        else if (random == 3)
-            p = Instantiate(_skull);
-
-        /*
-                if (random == 0)
-                    SpawnHammer();
-                else if (random == 1)
-                    SpawnHorseshoe();
-                else if (random == 2)
-                    SpawnBag();
-                else if (random == 3)
-                    SpawnSkull();
-                else
-                    SpawnCoin();
-        */
-        _battlePickupManager.SpawnPickup(p, transform.position);
-    }
-
-    void SpawnHammer()
-    {
-        if (_hammer == null) return;
-        Hammer instance = Instantiate(_hammer);
-        BattlePickup hammer = Instantiate(_hammer.Prefab, transform.position, Quaternion.identity)
-                            .GetComponent<BattlePickup>();
-        // hammer.Initialize(instance);
-
-        // Destroy(gameObject, 7f);
-    }
-
-    void SpawnHorseshoe()
-    {
-        if (_horseshoe == null) return;
-        Horseshoe instance = Instantiate(_horseshoe);
-        BattlePickup horseshoe = Instantiate(_horseshoe.Prefab, transform.position, Quaternion.identity)
-                            .GetComponent<BattlePickup>();
-        // horseshoe.Initialize(instance);
-
-        // Destroy(gameObject, 7f);
-    }
-
-    void SpawnBag()
-    {
-        if (_bag == null) return;
-        Bag instance = Instantiate(_bag);
-        BattlePickup bag = Instantiate(_bag.Prefab, transform.position, Quaternion.identity)
-                            .GetComponent<BattlePickup>();
-        // bag.Initialize(instance);
-
-        // Destroy(gameObject, 7f);
-    }
-
-    void SpawnSkull()
-    {
-        if (_skull == null) return;
-        Skull instance = Instantiate(_skull);
-        BattlePickup skull = Instantiate(_skull.Prefab, transform.position, Quaternion.identity)
-                            .GetComponent<BattlePickup>();
-        // skull.Initialize(instance);
-
-        // Destroy(gameObject, 7f);
     }
 }
--- Assets/_Scripts/Battle/Managers/BattlePickupManager.cs
+++ Assets/_Scripts/Battle/Managers/BattlePickupManager.cs
@@ -6,15 +6,37 @@ public class BattlePickupManager : PoolManager<BattlePickup>
 {
     [SerializeField] BattlePickup _pickupPrefab;
 
+    [SerializeField] Coin _coin;
+    [SerializeField] Hammer _hammer;
+    [SerializeField] Horseshoe _horseshoe;
+    [SerializeField] Bag _bag;
+    [SerializeField] Skull _skull;
+
+
     public void Initialize()
     {
         CreatePool(_pickupPrefab.gameObject);
     }
 
-    public void SpawnPickup(Pickup pickup, Vector3 position)
+    public void SpawnPickup(Vector3 position)
     {
-        BattlePickup p = GetObjectFromPool();
-        p.Initialize(pickup, position);
+        // 1% chance of spawning hammer
+        // 1% chance of spawning horseshoe
+        // 98% chance of spawning coin
+        int random = Random.Range(0, 100);
+        Pickup p = Instantiate(_coin);
+
+        if (random == 0 || random == 1)
+            p = Instantiate(_hammer);
+        else if (random == 2 || random == 3)
+            p = Instantiate(_horseshoe);
+        else if (random == 4 || random == 5)
+            p = Instantiate(_bag);
+        else if (random == 6 || random == 7)
+            p = Instantiate(_skull);
+
+        BattlePickup battlePickup = GetObjectFromPool();
+        battlePickup.Initialize(p, position);
     }
 
     public void BagCollected()
