6c353ffb3e3240096af30137a894e15eb0df7759
blis <blis@hey.com>
2024-01-12 14:48:04 +0100

    wildbolt storm is alright!

---
+++ Assets/_Scripts/Battle/Hero/Abilities/Lightning/BattleAbilityWildboltStorm.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAbilityWildboltStorm : BattleAbility
+{
+    [SerializeField] GameObject _effect;
+
+    [SerializeField] GameObject _wildboltPrefab;
+    List<BattleWildbolt> _wildboltPool = new();
+
+    public override void Initialize(Ability ability, bool startAbility = true)
+    {
+        base.Initialize(ability, startAbility);
+        transform.localPosition = new Vector3(0, 0.5f, 0f);
+    }
+
+
+    protected override IEnumerator FireAbilityCoroutine()
+    {
+        yield return base.FireAbilityCoroutine();
+
+        _effect.SetActive(true);
+        _effect.transform.parent = transform;
+        Vector3 pos = transform.position;
+        pos.y = 0.1f;
+        _effect.transform.position = pos;
+        yield return new WaitForSeconds(0.6f);
+        _effect.transform.parent = _battleManager.EntityHolder;
+
+        int projectileCount = _ability.GetAmount();
+        for (int i = 0; i < projectileCount; i++)
+        {
+            BattleWildbolt projectile = GetInactiveProjectile();
+            projectile.transform.position = transform.position;
+
+            projectile.Fire(_ability);
+            // yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(2f);
+
+        _effect.SetActive(false);
+    }
+
+    BattleWildbolt InitializeProjectile()
+    {
+        GameObject instance = Instantiate(_wildboltPrefab, Vector3.zero, Quaternion.identity, BattleManager.Instance.EntityHolder);
+        instance.SetActive(true);
+
+        BattleWildbolt projectile = instance.GetComponent<BattleWildbolt>();
+        projectile.Initialize(0);
+        _wildboltPool.Add(projectile);
+        return projectile;
+    }
+
+    BattleWildbolt GetInactiveProjectile()
+    {
+        foreach (BattleWildbolt p in _wildboltPool)
+            if (!p.gameObject.activeSelf)
+                return p;
+        return InitializeProjectile();
+    }
+}
---
+++ Assets/_Scripts/Battle/Hero/Abilities/Lightning/BattleWildbolt.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
+
+public class BattleWildbolt : BattleProjectile
+{
+    Rigidbody _rb;
+
+
+    public override void Initialize(int team)
+    {
+        base.Initialize(team);
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public void Fire(Ability ability)
+    {
+        _ability = ability;
+        EnableProjectile();
+        StartCoroutine(FireCoroutine());
+    }
+
+    public IEnumerator FireCoroutine()
+    {
+        float endTime = Time.time + _ability.GetDuration();
+        while (Time.time < endTime)
+        {
+            // choose a random direction but keep y = 0
+            Vector3 dir = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+            transform.rotation = Quaternion.LookRotation(dir);
+
+            _speed = Random.Range(5f, 15f);
+            yield return GoForward(dir, Random.Range(0.5f, 2f));
+        }
+        yield return Explode(transform.position);
+    }
+
+    IEnumerator GoForward(Vector3 dir, float timeInSeconds)
+    {
+        float t = 0;
+        while (t <= timeInSeconds)
+        {
+            transform.position += _speed * Time.fixedDeltaTime * dir;
+            t += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+}
