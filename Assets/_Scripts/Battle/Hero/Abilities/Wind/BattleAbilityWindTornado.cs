6585b0ea7eee790fcf0e2192d24dfa43baa09281
blis <blis@hey.com>
2024-01-16 19:42:48 +0100

    wind tornado, new steup

--- Assets/_Scripts/Battle/Hero/Abilities/Wind/BattleAbilityWindTornado.cs
+++ Assets/_Scripts/Battle/Hero/Abilities/Wind/BattleAbilityWindTornado.cs
@@ -5,8 +5,6 @@ using DG.Tweening;
 
 public class BattleAbilityWindTornado : BattleAbility
 {
-    [SerializeField] GameObject _tornadoPrefab;
-    List<BattleWindTornado> _tornadoPool = new();
 
     public override void Initialize(Ability ability, bool startAbility = true)
     {
@@ -23,26 +21,9 @@ public class BattleAbilityWindTornado : BattleAbility
             Vector3 pos = new(transform.position.x, 0, transform.position.z);
             Quaternion q = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
 
-            BattleWindTornado tornado = GetInactiveTornado();
-            tornado.Fire(pos, q);
+            BattleWindTornado tornado = GetInactiveAbilityObject() as BattleWindTornado;
+            tornado.Execute(pos, q);
             yield return new WaitForSeconds(0.2f);
         }
-    }
-
-    BattleWindTornado InitializeTornado()
-    {
-        GameObject instance = Instantiate(_tornadoPrefab, Vector3.zero, Quaternion.identity, _battleManager.AbilityHolder);
-        BattleWindTornado tornado = instance.GetComponent<BattleWindTornado>();
-        tornado.Initialize(_ability);
-        _tornadoPool.Add(tornado);
-        return tornado;
-    }
-
-    BattleWindTornado GetInactiveTornado()
-    {
-        foreach (BattleWindTornado p in _tornadoPool)
-            if (!p.gameObject.activeSelf)
-                return p;
-        return InitializeTornado();
     }
 }
--- Assets/_Scripts/Battle/Hero/Abilities/Wind/BattleWindTornado.cs
+++ Assets/_Scripts/Battle/Hero/Abilities/Wind/BattleWindTornado.cs
@@ -3,27 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class BattleWindTornado : MonoBehaviour
+public class BattleWindTornado : BattleAbilityObject
 {
+    float _speed = 5f;
 
-    [SerializeField] float _speed = 5f;
-    Ability _ability;
-
-    public void Initialize(Ability ability)
-    {
-        _ability = ability;
-    }
-    
-    public void Fire(Vector3 pos, Quaternion q)
+    public override void Execute(Vector3 pos, Quaternion q)
     {
         pos.y = 0;
-        transform.SetPositionAndRotation(pos, q);
-        gameObject.SetActive(true);
-
-        StartCoroutine(FireCoroutine());
+        base.Execute(pos, q);
     }
 
-    IEnumerator FireCoroutine()
+    protected override IEnumerator ExecuteCoroutine()
     {
         transform.localScale = Vector3.one * _ability.GetScale();
 
