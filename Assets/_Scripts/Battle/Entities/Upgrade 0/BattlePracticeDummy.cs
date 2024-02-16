d28070283ccfde27ee2cc5cc650d9d976cd907c4
blis <bernard5lis@gmail.com>
2024-02-16 12:41:46 +0100

    practice dummy ability is correct

--- Assets/_Scripts/Battle/Entities/Upgrade 0/BattlePracticeDummy.cs
+++ Assets/_Scripts/Battle/Entities/Upgrade 0/BattlePracticeDummy.cs
@@ -22,30 +22,30 @@ namespace Lis
 
         protected override IEnumerator CreatureAbility()
         {
-            Debug.Log($"ability");
             if (!IsOpponentInRange()) yield break;
-
-            yield return transform.DODynamicLookAt(Opponent.transform.position, 0.2f, AxisConstraint.Y);
-            yield return base.CreatureAbility();
-            CurrentAttackCooldown = Creature.AttackCooldown.GetValue();
-
-            _abilityEffectInstance = Instantiate(_abilityEffect, transform.position, Quaternion.identity);
+            Vector3 pos = transform.position;
+            pos.y = 0;
+            _abilityEffectInstance = Instantiate(_abilityEffect, pos, Quaternion.identity);
             _abilityEffectInstance.transform.parent = Gfx.transform;
 
-            Collider[] colliders = Physics.OverlapSphere(transform.position, _abilityEffectRadius);
-            foreach (Collider collider in colliders)
-            {
-                if (collider.TryGetComponent(out BattleEntity entity))
-                {
-                    if (entity.Team == Team) continue; // splash damage is player friendly
-                    if (entity.IsDead) continue;
+            transform.DODynamicLookAt(Opponent.transform.position, 0.2f, AxisConstraint.Y);
+            StartCoroutine(base.CreatureAbility());
+            CurrentAttackCooldown = Creature.AttackCooldown.GetValue();
 
-                    StartCoroutine(entity.GetHit(this, Creature.Power.GetValue() * 2));
-                    Quaternion q = Quaternion.Euler(0, -90, 0); // face default camera position
-                    GameObject hitInstance = Instantiate(Creature.HitPrefab, entity.Collider.bounds.center, q);
-                    hitInstance.transform.parent = Opponent.transform;
-                    _hitInstances.Add(hitInstance);
-                }
+            Collider[] results = new Collider[10];
+            Physics.OverlapSphereNonAlloc(pos, _abilityEffectRadius, results);
+            foreach (Collider r in results)
+            {
+                if (r == null) continue;
+                if (!r.TryGetComponent(out BattleEntity entity)) continue;
+                if (entity.Team == Team) continue; // splash damage is player friendly
+                if (entity.IsDead) continue;
+
+                StartCoroutine(entity.GetHit(this, Creature.Power.GetValue() * 2));
+                Quaternion q = Quaternion.Euler(0, -90, 0); // face default camera position
+                GameObject hitInstance = Instantiate(Creature.HitPrefab, entity.Collider.bounds.center, q);
+                hitInstance.transform.parent = Opponent.transform;
+                _hitInstances.Add(hitInstance);
             }
 
             Invoke(nameof(CleanUp), 2f);
