a3555e44331c1ba9a5f6ca4ca59de51b8922ab85
blis <bernard5lis@gmail.com>
2024-05-09 11:51:46 +0200

    poison bite

---
+++ Assets/_Scripts/Units/Attack/AttackControllerPoisonBite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Lis.Units.Attack
+{
+    public class AttackControllerPoisonBite : AttackController
+    {
+        [SerializeField] GameObject _effect;
+
+        public override IEnumerator AttackCoroutine()
+        {
+            while (CurrentCooldown > 0) yield return new WaitForSeconds(0.1f);
+            if (!IsOpponentInRange()) yield break;
+            BaseAttack();
+
+            _effect.transform.position = UnitController.Opponent.transform.position;
+            _effect.SetActive(true);
+
+            yield return UnitController.Opponent.GetHit(Attack);
+            Invoke(nameof(CleanUp), 2f);
+        }
+
+        void CleanUp()
+        {
+            _effect.SetActive(false);
+        }
+    }
+}
